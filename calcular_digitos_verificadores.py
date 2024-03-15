41c53a99c6ea1148f4cac8511140a6a87ebd49ae
Leandro Freitas <79721390+leandrofreitascadmus@users.noreply.github.com>
2024-03-15 14:43:09 -0300

    Update calcular_digitos_verificadores.py

--- calcular_digitos_verificadores.py
+++ calcular_digitos_verificadores.py
@@ -21,3 +21,7 @@ cpf_base_input = input("Digite os nove primeiros dígitos do CPF: ")
 
 resultado = calcula_digitos_verificadores(cpf_base_input)
 print(f"Os dois últimos dígitos do CPF são: {resultado}")
+
+
+
+
